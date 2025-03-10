8a6afb919b931a18be2c4d01734ba6a91da7ec29
kyungboshim <kyungbo.shim@neowiz.com>
2025-03-10 17:09:55 +0900

    test

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
           name: "AdiscopeMediaMax",
-          url: "https://github.com/adiscope/Adiscope-iOS-Developer/releases/download/2.0.11/AdiscopeMediaMax.xcframework.zip",
+          url: "https://github.com/adiscope/Adiscope-iOS-Developer/releases/download/2.0.12/AdiscopeMediaMax.xcframework.zip",
           checksum: "fa118d1a6a29b5885f7701da8efa63d1a194ef473f3218d63ffe677e31c878ce"
         ),
     ]
