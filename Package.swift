75ebc868a1eb821a7ea4c34e2c3e11e690c73981
kyungboshim <kyungbo.shim@neowiz.com>
2025-03-20 11:08:09 +0900

    test

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/AppLovin/AppLovin-MAX-Swift-Package.git", exact: "13.1.0"),
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", exact: "12.2.0"),
         .package(url: "https://github.com/Vungle/VungleAdsSDK-SwiftPackageManager.git", exact: "7.4.4"),
     ],
     targets: [
@@ -25,6 +26,11 @@ let package = Package(
             .target(name: "AdiscopeMediaVungle"),
             .target(name: "AdiscopeMediaMaxAdapterVungle"),
             .product(name: "VungleAdsSDK", package: "VungleAdsSDK-SwiftPackageManager"),
+            .target(name: "AdiscopeMediaAdManager"),
+            .target(name: "AdiscopeMediaAdMob"),
+            .target(name: "AdiscopeMediaMaxAdapterAdManager"),
+            .target(name: "AdiscopeMediaMaxAdapterAdMob"),
+            .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads"),
           ],
           path: "AdiscopeMediaMaxTarget"
         ),
@@ -32,6 +38,26 @@ let package = Package(
           name: "AdiscopeMediaMax",
           url: "https://github.com/adiscope/Adiscope-iOS-Developer/releases/download/2.0.17/AdiscopeMediaMax.xcframework.zip",
           checksum: "d138469ba12094f6dece70b4d4cc7bc58a22aac2a569a64958edbf80b2a72833"
+        ),
+        .binaryTarget(
+          name: "AdiscopeMediaAdManager",
+          url: "https://github.com/adiscope/Adiscope-iOS-Developer/releases/download/2.0.17/AdiscopeMediaAdManager.xcframework.zip",
+          checksum: "240cbbd71641f9f58012f40882940a07545f3c9f832316f9c7400663d6c6ba11"
+        ),
+        .binaryTarget(
+          name: "AdiscopeMediaAdMob",
+          url: "https://github.com/adiscope/Adiscope-iOS-Developer/releases/download/2.0.17/AdiscopeMediaAdMob.xcframework.zip",
+          checksum: "dba4cc1cab4be771a186455b0dd358bb9e0d2a27cffd7fa44a50e182d31b160d"
+        ),
+        .binaryTarget(
+          name: "AdiscopeMediaMaxAdapterAdManager",
+          url: "https://github.com/adiscope/Adiscope-iOS-Developer/releases/download/2.0.17/AdiscopeMediaMaxAdapterAdManager.xcframework.zip",
+          checksum: "92817361af9d39c422419d1180e1a160c0dc1cb9c43ef566d8c059985e7098f1"
+        ),
+        .binaryTarget(
+          name: "AdiscopeMediaMaxAdapterAdMob",
+          url: "https://github.com/adiscope/Adiscope-iOS-Developer/releases/download/2.0.17/AdiscopeMediaMaxAdapterAdMob.xcframework.zip",
+          checksum: "5d18f8f8a417f7efd33ff3ff55637fee2a42f2077353e1d5f65f5b7aeeda4564"
         ),
         .binaryTarget(
           name: "AdiscopeMediaVungle",
