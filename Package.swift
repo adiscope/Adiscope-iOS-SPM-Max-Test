4befaaba2bce193f3e8459d03e162c6064225f56
kyungboshim <kyungbo.shim@neowiz.com>
2025-03-20 10:02:56 +0900

    test

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/AppLovin/AppLovin-MAX-Swift-Package.git", exact: "13.1.0"),
-        .package(url: "https://github.com/bytedance/AdsGlobalPackage.git", exact: "6.5.0.8")
+//        .package(url: "https://github.com/bytedance/AdsGlobalPackage.git", exact: "6.5.0.8"),
     ],
     targets: [
         .target(
@@ -22,9 +22,9 @@ let package = Package(
           dependencies: [
             .target(name: "AdiscopeMediaMax"),
             .product(name: "AppLovinSDK", package: "AppLovin-MAX-Swift-Package"),
-            .target(name: "AdiscopeMediaPangle"),
-            .target(name: "AdiscopeMediaMaxAdapterPangle"),
-            .product(name: "Ads-Global", package: "AdsGlobalPackage"),
+//            .target(name: "AdiscopeMediaPangle"),
+//            .target(name: "AdiscopeMediaMaxAdapterPangle"),
+//            .product(name: "Ads-Global", package: "AdsGlobalPackage"),
           ],
           path: "AdiscopeMediaMaxTarget"
         ),
@@ -33,16 +33,16 @@ let package = Package(
           url: "https://github.com/adiscope/Adiscope-iOS-Developer/releases/download/2.0.17/AdiscopeMediaMax.xcframework.zip",
           checksum: "d138469ba12094f6dece70b4d4cc7bc58a22aac2a569a64958edbf80b2a72833"
         ),
-        .binaryTarget(
-          name: "AdiscopeMediaPangle",
-          url: "https://github.com/adiscope/Adiscope-iOS-Developer/releases/download/2.0.17/AdiscopeMediaPangle.xcframework.zip",
-          checksum: "2fdfe52f0158913d0d9e49f69daf3bcaf0dafb1b381a791caa3da29436d4f160"
-        ),
-        .binaryTarget(
-          name: "AdiscopeMediaMaxAdapterPangle",
-          url: "https://github.com/adiscope/Adiscope-iOS-Developer/releases/download/2.0.17/AdiscopeMediaMaxAdapterPangle.xcframework.zip",
-          checksum: "6bd4592e1711a544746b0f705c13f84cbefe8157f8a5a92858224ce2260dd102"
-        ),
+//        .binaryTarget(
+//          name: "AdiscopeMediaPangle",
+//          url: "https://github.com/adiscope/Adiscope-iOS-Developer/releases/download/2.0.17/AdiscopeMediaPangle.xcframework.zip",
+//          checksum: "2fdfe52f0158913d0d9e49f69daf3bcaf0dafb1b381a791caa3da29436d4f160"
+//        ),
+//        .binaryTarget(
+//          name: "AdiscopeMediaMaxAdapterPangle",
+//          url: "https://github.com/adiscope/Adiscope-iOS-Developer/releases/download/2.0.17/AdiscopeMediaMaxAdapterPangle.xcframework.zip",
+//          checksum: "6bd4592e1711a544746b0f705c13f84cbefe8157f8a5a92858224ce2260dd102"
+//        ),
 //        .binaryTarget(
 //          name: "InMobiSDK",
 //          url: "https://github.com/adiscope/Adiscope-iOS-Developer/releases/download/2.0.15/InMobiSDK.xcframework.zip",
