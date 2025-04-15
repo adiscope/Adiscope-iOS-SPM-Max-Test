3addcb1c0fadef69d95dec26452270364cf602dc
kyungboshim <kyungbo.shim@neowiz.com>
2025-04-15 18:25:57 +0900

    adevent

--- Package.swift
+++ Package.swift
@@ -20,6 +20,7 @@ let package = Package(
             name: "AdiscopeMediaMaxTarget",
             dependencies: [
                 .target(name: "AdiscopeMediaMax"),
+                .target(name: "AdiscopeAdEvent"),
                 .product(name: "AppLovinSDK", package: "AppLovin-MAX-Swift-Package"),
             ],
             path: "Sources"
@@ -28,6 +29,11 @@ let package = Package(
             name: "AdiscopeMediaMax",
             url: "https://github.com/adiscope/Adiscope-iOS-Developer/releases/download/2.1.0/AdiscopeMediaMax.zip",
             checksum: "1d9862146d023b022db802a725f1f8373dfd65c3a64193b8be10924592fa57d4"
+        ),
+        .binaryTarget(
+            name: "AdiscopeAdEvent",
+            url: "https://github.com/adiscope/Adiscope-iOS-Developer/releases/download/2.1.0/AdiscopeAdEvent.zip",
+            checksum: "69d053c142cd116ccf7b068a9e1c3be62895653127861851ace4982bb4684d03"
         ),
     ]
 )
