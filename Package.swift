5983f1c957892a23ec3ca882069073d534f4be3e
kyungboshim <kyungbo.shim@neowiz.com>
2025-04-15 18:05:28 +0900

    max test

--- Package.swift
+++ Package.swift
@@ -9,24 +9,25 @@ let package = Package(
     products: [
         .library(
             name: "AdiscopeMediaMax",
-            targets: ["AdiscopeMediaMaxTarget"]),
+            targets: ["AdiscopeMediaMaxTarget"]
+        ),
     ],
     dependencies: [
-        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", exact: "12.2.0"),
+        .package(url: "https://github.com/AppLovin/AppLovin-MAX-Swift-Package.git", exact: "13.1.0"),
     ],
     targets: [
         .target(
             name: "AdiscopeMediaMaxTarget",
             dependencies: [
                 .target(name: "AdiscopeMediaMax"),
-                .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads"),
+                .product(name: "AppLovinSDK", package: "AppLovin-MAX-Swift-Package"),
             ],
             path: "Sources"
         ),
         .binaryTarget(
             name: "AdiscopeMediaMax",
-            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/4.2.0/AdiscopeMediaAdManager.zip",
-            checksum: "d4532298f790bf12f64a74792243853b541bff4cbb1153546aa9814dd81793f8"
+            url: "https://github.com/adiscope/Adiscope-iOS-Developer/releases/download/2.1.0/AdiscopeMediaMax.zip",
+            checksum: "1d9862146d023b022db802a725f1f8373dfd65c3a64193b8be10924592fa57d4"
         ),
     ]
 )
